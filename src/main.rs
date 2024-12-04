09707e7e1eb2e7ad073ef4be3dba235fb9588133
MichaelScofield111 <141600921+MichaelScofield111@users.noreply.github.com>
2024-12-04 23:05:13 +0800

    build free standing rust binary

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+// no std
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// 这个函数将在 panic 时被调用
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+/// rebuild start point
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
