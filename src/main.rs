d87dc538979c45ed2734eb354122a560bf3f0d4b
MichaelScofield111 <141600921+MichaelScofield111@users.noreply.github.com>
2025-01-13 22:18:29 +0800

    add lib.rs

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,25 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use rust_os::println;
 
-mod serial;
-mod vga_buffers;
-/// 这个函数将在 panic 时被调用
+/// Print an Error Message on Panic
+///our panic handler in test mode
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello, World! Rust-os~{}", "!");
+    // Conditional Compilation
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+/// This function is called on panic.
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -17,99 +28,8 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-/// Print an Error Message on Panic
-///our panic handler in test mode
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitcode::Fail);
-    loop {}
-}
-static HELLO: &[u8] = b"Hello, World! Rust-os~";
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-/// rebuild start point
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    // let vga_buffer = 0xb8000 as *mut u8;
-
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    //     }
-    // }
-    // test
-    //vga_buffers::print_something();
-
-    use core::fmt::Write;
-    println!("test println macro");
-    vga_buffers::WRITER.lock().write_str("Hello rust-os!");
-    write!(
-        vga_buffers::WRITER.lock(),
-        ", some numbers: {} {}",
-        42,
-        1.337
-    )
-    .unwrap();
-    println!();
-    // Conditional Compilation
-    #[cfg(test)]
-    test_main();
-
-    loop {}
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    /// new
-    exit_qemu(QemuExitcode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(1, 1);
-    serial_println!("[ok]");
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitcode {
-    Success = 0x10,
-    Fail = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitcode) {
-    use x86_64::instructions::port::Port;
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+    rust_os::test_panic_handler(info)
 }
